587f72c6642e3d631964ec4ffeca66dd07a2a1c4
xiaohuits <hui.xiao@uconn.edu>
2020-12-09 17:15:13 -0800

    Adding service

--- src/gripper_node.py
+++ src/gripper_node.py
@@ -3,6 +3,7 @@
 import rospy 
 from std_msgs.msg import Int32
 import robotiq_gripper
+from robotiq_hande_ros_driver.srv import gripper_service, gripper_serviceResponse
 
 class HandEGripper:
     def __init__(self):
@@ -19,6 +20,8 @@ class HandEGripper:
         self.speed = rospy.get_param('~speed', 255)
         self.force = rospy.get_param('~force', 255)
         self.sub = rospy.Subscriber("gripper_command", Int32, self.gripperCallback)
+        # set up server
+        self.gripper_server = rospy.Service('gripper_service', gripper_service, self.serverCallback)
     
     def gripperCallback(self, data):
         #rospy.loginfo(data.data)
@@ -28,6 +31,15 @@ class HandEGripper:
         if data.data == 1:
             rospy.loginfo("opening the gripper. Speed={}, force={}".format(self.speed, self.force))
             self.gripper.move_and_wait_for_pos(0, self.speed, self.force)
+    
+    def serverCallback(self, request):
+        if request.command == 0:
+            rospy.loginfo("closing the gripper. Speed={}, force={}".format(self.speed, self.force))
+            self.gripper.move_and_wait_for_pos(255, self.speed, self.force)
+        if request.command == 1:
+            rospy.loginfo("opening the gripper. Speed={}, force={}".format(self.speed, self.force))
+            self.gripper.move_and_wait_for_pos(0, self.speed, self.force)
+        return(gripper_serviceResponse(0))
 
 if __name__ == '__main__':
     gripper_obj = HandEGripper()
